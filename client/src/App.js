efd616d0a0812e0f0e2efdaab6719aaa63642d11
Sofiya Lazarova <sofiya@sofiya.ca>
2020-06-12 10:43:35 +0200

    feat: add chart with tabs and adjust layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,18 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import ApiClient from './ApiClient';
 import Nav from './components/Nav';
-import List from './components/List';
-import Options from './components/Options';
-import Charts from './components/Charts';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
   //get the data object via the api
   //
+  
 
   return(
     <div className="App">
       <Nav></Nav>
-      <Charts></Charts>
+      <Dashboard></Dashboard>
     </div>
   )
 }
---
+++ client/src/components/Charts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Charts.css';
+
+export default function Charts() {
+  return (
+    <div className="charts-container">
+    </div>
+  )
+}

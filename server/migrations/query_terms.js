d56e3a47f34f35275289d9c28eff0132f64a0355
Sofiya Lazarova <sofiya@sofiya.ca>
2020-06-16 10:30:20 +0200

    refactor: remove unused files from server

--- server/migrations/query_terms.js
+++ server/migrations/query_terms.js
@@ -9,14 +9,14 @@ queryTerms = {
   'Platforms': ['Linux', 'Windows', 'Docker', 'AWS'],
 }
 
-// longQueryTerms = {
-//   languages: ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java', 'Bash', 'C#', 'TypeScript', 'PHP', 'C++'],
+longQueryTerms = {
+  languages: ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java', 'Bash', 'C#', 'TypeScript', 'PHP', 'C++'],
 
-//   otherTools: ['Nodejs', '.NET', '.NET Core', 'Pandas', 'React Native', 'TensorFlow', 'Unity 3D', 'Ansible', 'Teraform', 'Flutter'],
+  otherTools: ['Nodejs', '.NET', '.NET Core', 'Pandas', 'React Native', 'TensorFlow', 'Unity 3D', 'Ansible', 'Teraform', 'Flutter'],
 
-//   databases: ['MySQL', 'PostgreSQL', 'Microsoft SQL Server', 'SQLite', 'MongoDB', 'Redis', 'MariaDB', 'Oracle', 'Elasticsearch', 'Firebase'],
+  databases: ['MySQL', 'PostgreSQL', 'Microsoft SQL Server', 'SQLite', 'MongoDB', 'Redis', 'MariaDB', 'Oracle', 'Elasticsearch', 'Firebase'],
 
-//   platforms: ['Linux', 'Windows', 'Docker', 'AWS', 'MacOS', 'Android', 'Microsoft Azure', 'Google Cloud Platform', 'Raspberry Pi', 'WordPress'],
-// }
+  platforms: ['Linux', 'Windows', 'Docker', 'AWS', 'MacOS', 'Android', 'Microsoft Azure', 'Google Cloud Platform', 'Raspberry Pi', 'WordPress'],
+}
 
-module.exports = {queryTerms};
+module.exports = {queryTerms, longQueryTerms};

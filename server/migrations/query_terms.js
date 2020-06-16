1bf161699833d7f557fea12b0b65103d61555952
Sofiya Lazarova <sofiya@sofiya.ca>
2020-06-16 10:21:26 +0200

    feat: fetch data via Twitter node API, store to db

---
+++ server/migrations/query_terms.js
@@ -0,0 +1,22 @@
+
+queryTerms = {
+  'Languages': ['JavaScript', 'HTML/CSS', 'SQL', 'Python'],
+
+  'Other Tools': ['Node.js', 'NET Core', 'Pandas', 'React Native'],
+
+  'Databases': ['MySQL', 'PostgreSQL', 'Microsoft SQL Server', 'SQLite'],
+
+  'Platforms': ['Linux', 'Windows', 'Docker', 'AWS'],
+}
+
+// longQueryTerms = {
+//   languages: ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java', 'Bash', 'C#', 'TypeScript', 'PHP', 'C++'],
+
+//   otherTools: ['Nodejs', '.NET', '.NET Core', 'Pandas', 'React Native', 'TensorFlow', 'Unity 3D', 'Ansible', 'Teraform', 'Flutter'],
+
+//   databases: ['MySQL', 'PostgreSQL', 'Microsoft SQL Server', 'SQLite', 'MongoDB', 'Redis', 'MariaDB', 'Oracle', 'Elasticsearch', 'Firebase'],
+
+//   platforms: ['Linux', 'Windows', 'Docker', 'AWS', 'MacOS', 'Android', 'Microsoft Azure', 'Google Cloud Platform', 'Raspberry Pi', 'WordPress'],
+// }
+
+module.exports = {queryTerms};
---
+++ server/twitter_api.js
@@ -0,0 +1,130 @@
+const https = require('https');
+const request = require('request');
+const util = require('util');
+
+require('dotenv').config();
+const db = require('./models');
+const Technology = db.technology;
+const Count = db.count;
+
+const get = util.promisify(request.get);
+const post = util.promisify(request.post);
+
+const consumer_key = process.env.TWITTER_CONSUMER_KEY; // Add your API key here
+const consumer_secret = process.env.TWITTER_CONSUMER_SECRET; // Add your API secret key here
+
+const bearerTokenURL = new URL('https://api.twitter.com/oauth2/token');
+const searchURL = new URL('https://api.twitter.com/labs/2/tweets/search');
+
+async function bearerToken (auth) {
+  const requestConfig = {
+    url: bearerTokenURL,
+    auth: {
+      user: consumer_key,
+      pass: consumer_secret,
+    },
+    form: {
+      grant_type: 'client_credentials',
+    },
+  };
+  
+  
+  const response = await post(requestConfig);
+  return JSON.parse(response.body).access_token;
+}
+
+(async () => {
+  let token;
+  const maxResults = 100;
+  
+  //create end_time that's one hour ago
+  var tempEnd = new Date();
+  tempEnd.setHours(tempEnd.getHours()-1);
+  var end_time = new Date(tempEnd).toISOString();
+  
+  //create start_time that's one day and one hour ago
+  var tempStart = new Date();
+  tempStart.setHours(tempStart.getHours()-2);
+  // tempStart.setDate(tempStart.getDate()-1);
+  var start_time = new Date(tempStart).toISOString();
+  
+  try {
+    // Exchange credentials for a Bearer token
+    token = await bearerToken({consumer_key, consumer_secret});
+  } catch (e) {
+    console.error(`Could not generate a Bearer token. Please check that your credentials are correct and that the Filtered Stream preview is enabled in your Labs dashboard. (${e})`);
+    process.exit(-1);
+  }
+  
+  const requestConfig = {
+    url: searchURL,
+    qs: {
+      max_results: maxResults,
+      start_time: start_time,
+      end_time: end_time
+    },
+    auth: {
+      bearer: token,
+    },
+    headers: {
+      'User-Agent': 'LabsRecentSearchQuickStartJS',
+    },
+    json: true,
+  };
+  
+  async function getCount(requestConfig) {
+    try {
+      let res = await get(requestConfig);
+      // console.log(res.statusCode);
+      // console.log(res.body);
+      if (res.statusCode !== 200) {
+        throw new Error(res.json);
+        return;
+      }
+      return res.body;
+    } catch (error) {
+      console.error(`Could not get search results. An error occurred: ${error}`);
+      process.exit(-1)
+    }
+  }
+  
+  //find all technologies from the technologies table
+  const technologies = await Technology.findAll(); 
+  
+  for (let i = 0; i < technologies.length; i++) {
+    let query = technologies[i].dataValues.name;
+    requestConfig.qs.query = encodeURIComponent(query);
+    
+    let totalCount = 0;
+    let body = await getCount(requestConfig);
+    let resultCount = body.meta.result_count;
+    
+    if (resultCount) {
+      totalCount += resultCount;
+      let nextToken = body.meta.next_token;
+      
+      if (nextToken) {
+        
+        requestConfig.qs.next_token = nextToken;
+        
+        while(nextToken) {
+          let next_body = await getCount(requestConfig);
+          totalCount += next_body.meta.result_count;
+          requestConfig.qs.next_token = next_body.meta.next_token;
+          nextToken = next_body.meta.next_token;
+          console.log('totalCount', totalCount);
+        }
+      }
+    }
+    console.log(query, totalCount);
+
+    try {
+      await technologies[i].createCount({
+        total: totalCount,
+      })
+      console.log(`Success inserting into db`);
+    } catch (error) {
+      console.log(`Error inserting ${query} into db`, error); 
+    }
+  }
+})();

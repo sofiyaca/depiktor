b8925709fdbc16637f6ca5344c8dd4d859bd5f3f
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-19 08:27:01 +0000

    refactor: moved options-container from dashboard into its own component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,21 +1,22 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ApiClient from '../../services/ApiClient';
 import './Dashboard.css';
 import Spinner from '../spinner/Spinner';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './../react-tabs/React-Tabs.css';
-import {Line, Bar, Radar} from 'react-chartjs-2';
-import Select from 'react-select';
-import ReactSlider from 'react-slider';
+import { Line, Bar, Radar } from 'react-chartjs-2';
 
 import mockData from './../../mock-data/mockData.json';
+import OptionsContainer from '../options-container/OptionsContainer';
 
 export default function Dashboard() {
-
   const [loadStatus, setLoadStatus] = useState(true);
   const [technologies, setTechnologies] = useState({});
-  const [selectLabel, setSelectLabel] = useState({value: 'line', label: 'Line'});
+  const [selectLabel, setSelectLabel] = useState({
+    value: 'line',
+    label: 'Line',
+  });
   const [selectTime, setSelectTime] = useState(0);
   const [maxLabel, setMaxLabel] = useState(0);
 
@@ -25,112 +26,82 @@ export default function Dashboard() {
     setLoadStatus(false);
   }, []);
 
-    // ApiClient.getTechnologies()
-    //   .then(technologies => {setTechnologies(technologies); setMaxLabel(technologies.Platforms.labels.length);})
-    //   .then(()=> setLoadStatus(false))
-    // }, []);
+  // ApiClient.getTechnologies()
+  //   .then(technologies => {setTechnologies(technologies); setMaxLabel(technologies.Platforms.labels.length);})
+  //   .then(()=> setLoadStatus(false))
+  // }, []);
 
   function handleSelectedLabel(e) {
     setSelectLabel(e);
   }
 
   function handleSelectedTime(e) {
-    console.log(e);
     setSelectTime(e);
   }
 
-const chartOptions = [
-  { value: 'line', label: 'Line' },
-  { value: 'bar', label: 'Bar' },
-  { value: 'radar', label: 'Radar' }
-];
+  const chartOptions = [
+    { value: 'line', label: 'Line' },
+    { value: 'bar', label: 'Bar' },
+    { value: 'radar', label: 'Radar' },
+  ];
 
-const chartJSOptions = {
-  responsive: true,
+  const chartJSOptions = {
+    responsive: true,
     scales: {
-      xAxes: [{
-        type: 'time',
-        time: {
-          unit: 'hour',
-          unitStepSize: 1,
-          displayFormats: {
-             'hour': 'hA'
-          }
-        }
-      }]
-    }
-  }
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            unit: 'hour',
+            unitStepSize: 1,
+            displayFormats: {
+              hour: 'hA',
+            },
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <div className="dashboard">
+      <OptionsContainer
+        handleSelectedLabel={handleSelectedLabel}
+        handleSelectedTime={handleSelectedTime}
+        chartOptions={chartOptions}
+        selectLabel={selectLabel}
+        selectTime={selectTime}
+        maxLabel={maxLabel}
+      />
+      <div className="tabs-container">
+        {!loadStatus ? (
+          <Tabs>
+            <TabList>
+              {Object.keys(technologies).map((techType) => (
+                <Tab key={'tab-' + techType}> {techType} </Tab>
+              ))}
+            </TabList>
 
-    <div className="options-container">
-      <div className="chart-options-container">
-        <div className="options-header">
-          <p>Options</p>
-        </div>
-
-        <div className="chart-style-container">
-          <p>Chart Style</p>
-          <Select options={chartOptions} className="chart-style-select-dropdown" 
-          placeholder="Select Chart Style..." value={selectLabel} onChange={handleSelectedLabel}>
-
-          </Select>
-        </div>
-
-        <div className="chart-timeframe-container">
-        <p>Timeframe</p>
-          <ReactSlider 
-            className="horizontal-slider"
-            thumbClassName="options-thumb"
-            trackClassName="options-track"
-            min={0}
-            max={maxLabel}
-            // invert={true}
-            value={selectTime}
-            onAfterChange={handleSelectedTime}
-            renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}>    
-          </ReactSlider>
-        </div>
+            {Object.values(technologies).map((techType) => (
+              <TabPanel key={'panel-' + techType.labels[0]}>
+                {
+                  {
+                    Line: (
+                      <Line data={techType} options={chartJSOptions}></Line>
+                    ),
+                    Bar: <Bar data={techType} options={chartJSOptions}></Bar>,
+                    Radar: (
+                      <Radar data={techType} options={chartJSOptions}></Radar>
+                    ),
+                  }[selectLabel.label]
+                }
+              </TabPanel>
+            ))}
+          </Tabs>
+        ) : (
+          <Spinner />
+        )}
       </div>
-
     </div>
-
-    <div className="tabs-container">
-
-      { !loadStatus ?
-        <Tabs>
-    
-          <TabList>
-          {
-          Object.keys(technologies).map(techType => 
-            <Tab key={'tab-'+ techType}> {techType} </Tab>
-            )
-          }
-          </TabList>
-
-        {
-          Object.values(technologies).map(techType => 
-            <TabPanel key={'panel-' + techType.labels[0]} >
-              {{
-                'Line': (<Line data={techType} options={chartJSOptions}></Line>),
-                'Bar': (<Bar data={techType} options={chartJSOptions}></Bar>),
-                'Radar': (<Radar data={techType} options={chartJSOptions}></Radar>)
-              }[selectLabel.label]}
-
-            </TabPanel>
-            )
-        }
-        </Tabs>
-        :
-        <Spinner/>
-      }
-
-    </div>
-
-  </div>
-    )
-  }
-
-
-  
+  );
+}

1894b7dd3cc9d6ee238cf5fd6f07668759d44058
Juno <Laevino@gmail.com>
2020-06-19 06:28:50 -0400

    test: added basic render test to OptionsContainer, and tested for correct titles.

---
+++ client/src/components/options-container/OptionsContainer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OptionsContainer from './OptionsContainer';
+import { render } from '@testing-library/react';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<OptionsContainer />, div);
+});
+
+it('renders titles correctly', () => {
+  const { getByText } = render(<OptionsContainer />);
+
+  expect(getByText('Options')).toBeInTheDocument();
+  expect(getByText('Chart Style')).toBeInTheDocument();
+  expect(getByText('Timeframe')).toBeInTheDocument();
+});

c67359ad7eb06a4e86017b3880aedbc38603039e
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-20 13:50:39 +0000

    test: add snapshot testing to options-container

--- client/src/components/options-container/OptionsContainer.test.js
+++ client/src/components/options-container/OptionsContainer.test.js
@@ -1,71 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import OptionsContainer from './OptionsContainer';
 import mocks from './OptionsContainer.mock';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
-describe('renders without crashing', () => {
-  it('renders select correctly', () => {
-    render(
-      <OptionsContainer
-        handleSelectedLabel={mocks.handleSelectedLabel}
-        handleSelectedTime={mocks.handleSelectedTime}
-        chartOptions={mocks.chartOptions}
-        selectLabel={mocks.chartOptions.bar.label}
-        selectTime={0}
-        maxLabel={3}
-      />
-    );
+it('renders without crashing', () => {
+  render(
+    <OptionsContainer
+      handleSelectedLabel={mocks.handleSelectedLabel}
+      handleSelectedTime={mocks.handleSelectedTime}
+      chartOptions={mocks.chartOptions}
+      selectLabel={mocks.chartOptions.bar.label}
+      selectTime={0}
+      maxLabel={3}
+    />
+  );
+});
 
-    render(
-      <OptionsContainer
-        handleSelectedLabel={mocks.handleSelectedLabel}
-        handleSelectedTime={mocks.handleSelectedTime}
-        chartOptions={mocks.chartOptions}
-        selectLabel={mocks.chartOptions.line.label}
-        selectTime={0}
-        maxLabel={3}
-      />
-    );
-
-    render(
-      <OptionsContainer
-        handleSelectedLabel={mocks.handleSelectedLabel}
-        handleSelectedTime={mocks.handleSelectedTime}
-        chartOptions={mocks.chartOptions}
-        selectLabel={mocks.chartOptions.radar.label}
-        selectTime={0}
-        maxLabel={3}
-      />
-    );
-  });
-
-  it('renders slider correctly', () => {
-    render(
-      <OptionsContainer
-        handleSelectedLabel={mocks.handleSelectedLabel}
-        handleSelectedTime={mocks.handleSelectedTime}
-        chartOptions={mocks.chartOptions}
-        selectLabel={mocks.chartOptions.bar.label}
-        selectTime={10}
-        maxLabel={5}
-      />
-    );
-
-    render(
-      <OptionsContainer
-        handleSelectedLabel={mocks.handleSelectedLabel}
-        handleSelectedTime={mocks.handleSelectedTime}
-        chartOptions={mocks.chartOptions}
-        selectLabel={mocks.chartOptions.radar.label}
-        selectTime={30}
-        maxLabel={7}
-      />
-    );
-  });
-
-  it('renders titles correctly', () => {
-    const { getByText } = render(
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
       <OptionsContainer
         handleSelectedLabel={mocks.handleSelectedLabel}
         handleSelectedTime={mocks.handleSelectedTime}
@@ -74,25 +28,8 @@ describe('renders without crashing', () => {
         selectTime={0}
         maxLabel={3}
       />
-    );
+    )
+    .toJSON();
 
-    expect(getByText('Options')).toBeInTheDocument();
-    expect(getByText('Chart Style')).toBeInTheDocument();
-    expect(getByText('Timeframe')).toBeInTheDocument();
-  });
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(
-      <OptionsContainer
-        handleSelectedLabel={mocks.handleSelectedLabel}
-        handleSelectedTime={mocks.handleSelectedTime}
-        chartOptions={mocks.chartOptions}
-        selectLabel={mocks.chartOptions.line.label}
-        selectTime={0}
-        maxLabel={3}
-      />,
-      div
-    );
-  });
+  expect(tree).toMatchSnapshot();
 });

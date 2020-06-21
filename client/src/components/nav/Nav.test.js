8e4903b11288bc7c102f17b0389fc801d5833889
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-21 18:18:44 +0000

    test: add render and snapshot tests to navbar component

--- client/src/components/nav/Nav.test.js
+++ client/src/components/nav/Nav.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
 import Nav from './Nav';
+import { cleanup, render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 afterEach(cleanup);
 
-test('Navbar Renders depiktor', () => {
+it('renders without crashing', () => {
+  render(<Nav />);
+});
+
+it('renders text depictor', () => {
   const { getByText } = render(<Nav />);
   expect(getByText('depiktor')).toBeInTheDocument();
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Nav />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
